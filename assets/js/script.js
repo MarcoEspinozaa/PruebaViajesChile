af5551d685d75b7a3c75d493feac0fd9a3bba130
MarcoEspinozaa <marcoo.espinozaa@gmail.com>
2022-01-20 22:10:15 -0300

    commit 95%

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,26 @@
 $(function (){
-    $('.carousel').carousel()
-    autoplay: true
-    autoplayTimeout: 1000
-    autoplaySpeed: 500
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+
+  console.log("funciona 1")
+
+  $(document).ready(function(){
+    $('.carousel').carousel({
+      interval: 2000
+    })
+  })
+
+  console.log("funciona 2")
+
+  $('a[data-slide="prev"]').click(function() {
+    $('#myCarousel').carousel('prev')
+  })
+  
+  $('a[data-slide="next"]').click(function() {
+    $('#myCarousel').carousel('next')
+  })
 
 })
